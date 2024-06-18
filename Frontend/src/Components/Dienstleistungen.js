b0bf2af687abe2a2f7674ec9de9be27e05e371e0
SaliduMaschine <111045919+DamianEisenring@users.noreply.github.com>
2024-06-18 15:12:17 +0200

    changes to css and story

--- Frontend/src/Components/Dienstleistungen.js
+++ Frontend/src/Components/Dienstleistungen.js
@@ -1,13 +1,23 @@
-// DienstleistungenPage.jsx
-import React from 'react';
+import React from "react";
+import "./Dienstleistungen.css";
 
-function Dienstleistungen() {
-    return (
-        <div>
-            <h1>Unsere Dienstleistungen</h1>
-            <p>Hier findest du Informationen über die Dienstleistungen, die wir anbieten.</p>
-        </div>
-    );
+function DienstleistungenPage() {
+  return (
+    <div className="container">
+      <h1>Unsere Dienstleistungen</h1>
+      <p>
+        Wir bieten eine Vielzahl von Dienstleistungen rund um den Pilzigel an,
+        beispielsweise die Pilzschere
+        <br /> <img src="./img/Pilzschere.jpeg" />{" "}
+      </p>{" "}
+      <br /> <br /> Den Igepilzhut <br /> <img src="./img/Pilzhut.jpeg" />
+      <p>
+      Pilzzahnbürste
+      <br /> 
+      </p>
+      <img src="./img/Pilzbürste.jpeg" />   
+    </div>
+  );
 }
 
-export default Dienstleistungen;
+export default DienstleistungenPage;
--- Frontend/src/Components/Kontakt.js
+++ Frontend/src/Components/Kontakt.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React from "react";
+import "./Kontakt.css";
 
-function Kontakt() {
-    return (
-        <div>
-            <h1>Kontakt</h1>
-            <p>Hier sind unsere Kontaktinformationen.</p>
-        </div>
-    );
+function KontaktPage() {
+  return (
+    <div className="container">
+      <h1>Kontakt</h1>
+      <p>
+        Igelify_ist_die_best_Firma@Igelify.nl <br /> 
+        +31 187420 69 69 <br />
+        Merowingerweg 1 <br />
+        5079 Zeihen <br />
+
+      </p>
+    </div>
+  );
 }
 
-export default Kontakt;
+export default KontaktPage;
